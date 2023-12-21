391f24f0c0eb2eade353be80b31c1edbfe686e6f
papusko <papuskone43@gmail.com>
2023-12-21 19:46:08 +0100

    redirect after registration

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -32,7 +32,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::LANDINGPAGE;
+    protected $redirectTo = '/';
 
     /**
      * Create a new controller instance.
